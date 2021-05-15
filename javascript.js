f54df3add122066b655d99074175c9c176327a23
Durgesh Prajapati <72651922+Drisa-Rokes@users.noreply.github.com>
2021-05-15 21:26:57 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+document.getElementById("demo").innerHTML="Hello world";
+function about(){
+	alert("You have click on about botton");
+}
+
+function moreabout(){
+	alert("Now website is under the construction , later on you can read more about");
+}
+
+function myFunction() {
+  var x = document.getElementById("formlogin");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function log() {
+	alert("Done");
+	// body...
+}
+
+function openWin() {
+  window.open("file.html");
+}
+
